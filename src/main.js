0a1b7afe44208944012c122f45c83a98a4eb1c15
vodoprovod <olegbobrovnik228@gmail.com>
2024-12-19 22:38:28 +0200

    add npm

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "./js/partials/header";
+import "./js/partials/hero";
+import "./js/partials/about-me";
+import "./js/partials/benefits";
+import "./js/partials/project";
+import "./js/partials/faq";
+import "./js/partials/covers";
+import "./js/partials/reviews";
+import "./js/partials/footer";
+
+
+
