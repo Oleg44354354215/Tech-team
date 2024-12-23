0ecdc87b9ce37dba04af4dda57cbc585b916e87b
Valeriia Martynova <vmartynova95@gmail.com>
2024-12-23 14:21:52 +0200

    change href for img, svg

--- src/js/partials/about-me.js
+++ src/js/partials/about-me.js
@@ -1,9 +1,9 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const accordionButtons = document.querySelectorAll('.accordion__button');
@@ -13,58 +13,54 @@ document.addEventListener('DOMContentLoaded', () => {
       const currentItem = button.parentElement;
       const isActive = currentItem.classList.contains('accordion__item--active');
       const svgUseElement = button.querySelector('svg use');
-     
+
       if (isActive) {
         currentItem.classList.remove('accordion__item--active');
-        svgUseElement.setAttribute('href', '../img/arrow.svg#IconClose'); 
+        svgUseElement.setAttribute('href', './img/sprite.svg#IconClose');
       } else {
         currentItem.classList.add('accordion__item--active');
-        svgUseElement.setAttribute('href', '../img/arrow.svg#IconOpen'); 
+        svgUseElement.setAttribute('href', './img/sprite.svg#IconOpen');
       }
     });
   });
-  
+
   const firstItem = document.querySelector('.accordion__item');
   if (firstItem) {
     firstItem.classList.add('accordion__item--active');
     const firstSvgUse = firstItem.querySelector('.accordion__button svg use');
     if (firstSvgUse) {
-      firstSvgUse.setAttribute('href', '../img/arrow.svg#IconOpen');
+      firstSvgUse.setAttribute('href', './img/sprite.svg#IconOpen');
     }
   }
+
+  /*const nextButton = document.querySelector('.swiper-button-next');*/
+
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Keyboard, Mousewheel],
+    slidesPerView: 2,
+    spaceBetween: 0,
+    loop: true,
+    speed: 800,
+    navigation: {
+      nextEl: '.swiper-button-next',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    mousewheel: {
+      invert: true,
+    },
+    breakpoints: {
+      375: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 6,
+      },
+    },
+  });
 });
-
-  
-  const swiper = new Swiper('.swiper', {
-      direction: 'horizontal',
-      slidesPerView: 2,
-      spaceBetween: 0,
-      loop: true,
-      slidesPerGroup: 2,
-      navigation: {
-        nextEl: '.swiper-button-next',        
-    },            
-      keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
-    mousewheel: {
-      sensitivity: 1,
-    },
-    autoHeight: true,
-    speed: 800,
-    effect: 'slide',
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1440: {
-        slidesPerView: 3,
-      },
-    }, 
-    });
-
