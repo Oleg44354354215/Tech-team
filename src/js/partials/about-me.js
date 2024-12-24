a8f61192c4098a0f90511d836adc4aa6a93789c2
Valeriia Martynova <vmartynova95@gmail.com>
2024-12-24 14:41:05 +0200

    update swiper

--- src/js/partials/about-me.js
+++ src/js/partials/about-me.js
@@ -11,28 +11,33 @@ document.addEventListener('DOMContentLoaded', () => {
   accordionButtons.forEach(button => {
     button.addEventListener('click', () => {
       const currentItem = button.parentElement;
+      const content = currentItem.querySelector('.accordion__content');
       const isActive = currentItem.classList.contains('accordion__item--active');
       const svgUseElement = button.querySelector('svg use');
 
-      if (isActive) {
+      if (isActive) {        
+        content.style.maxHeight = null;
         currentItem.classList.remove('accordion__item--active');
         svgUseElement.setAttribute('href', './img/sprite.svg#IconClose');
-      } else {
+      } else {        
+        content.style.maxHeight = `${content.scrollHeight}px`;
         currentItem.classList.add('accordion__item--active');
         svgUseElement.setAttribute('href', './img/sprite.svg#IconOpen');
       }
     });
   });
 
+  
   const firstItem = document.querySelector('.accordion__item');
   if (firstItem) {
+    const firstContent = firstItem.querySelector('.accordion__content');
     firstItem.classList.add('accordion__item--active');
+    firstContent.style.maxHeight = `${firstContent.scrollHeight}px`;
     const firstSvgUse = firstItem.querySelector('.accordion__button svg use');
     if (firstSvgUse) {
       firstSvgUse.setAttribute('href', './img/sprite.svg#IconOpen');
     }
   }
-
   
   const swiper = new Swiper('.swiper', {
     modules: [Navigation, Keyboard, Mousewheel],
@@ -47,9 +52,12 @@ document.addEventListener('DOMContentLoaded', () => {
       enabled: true,
       onlyInViewport: true,
     },
-    mousewheel: {
+    mousewheel: {      
       invert: true,
+      passive: true,
     },
+    touchEventsTarget: 'wrapper', 
+    createElements: true,
     breakpoints: {
       375: {
         slidesPerView: 2,
