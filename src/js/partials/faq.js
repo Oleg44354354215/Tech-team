cfaf86d33a894ad7ed154ac0e40242b8faa08df9
fediakin <fedrvoynr@gmail.com>
2024-12-26 13:08:48 +0200

    add-new-faq

--- src/js/partials/faq.js
+++ src/js/partials/faq.js
@@ -1,8 +1,53 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
 
-const accordion = new Accordion('.faq-list', {
-  duration: 250,
-  showMultiple: true,
-  triggerClass: 'faq-item-button',
+function createAccordion({
+  containerClass,
+  elementClass,
+  triggerClass,
+  panelClass,
+  openOnInit = [],
+  duration = 400,
+}) {
+  return new Accordion(containerClass, {
+    elementClass,
+    triggerClass,
+    panelClass,
+    showMultiple: true,
+    openOnInit: openOnInit,
+    duration: duration,
+  });
+}
+function handleAccordionClick(event, { btnClass, iconClass }) {
+  const arrowBtn =
+    event.currentTarget.querySelector(btnClass) ||
+    event.target.closest(btnClass);
+  const arrowIcon = event.currentTarget.querySelector(iconClass);
+  if (!arrowBtn) return;
+  const isRotated = arrowBtn.classList.contains('rotated');
+  if (isRotated) {
+    arrowIcon.style.transform = 'rotate(0deg)';
+    arrowBtn.classList.remove('rotated');
+  } else {
+    arrowIcon.style.transform = 'rotate(180deg)';
+    arrowBtn.classList.add('rotated');
+  }
+}
+
+const faqAccordion = document.querySelector('.faq-items');
+const options = {
+  containerClass: '.faq-items',
+  elementClass: 'faq-item',
+  triggerClass: 'faq-title',
+  panelClass: 'faq-descr',
+};
+const clickOptions = {
+  btnClass: '.faq-acordeon-btn',
+  iconClass: '.modal-btn-icon',
+};
+const faqAccordionTriggers = faqAccordion.querySelectorAll('.faq-title');
+faqAccordionTriggers.forEach(accordionTrigger => {
+  accordionTrigger.addEventListener('click', event => {
+    handleAccordionClick(event, clickOptions);
+  });
 });
+createAccordion(options);
